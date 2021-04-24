51282797b9a8dff076ed5dd4cf37c2eaa45e286d
suman Islam <sumunahmed1416@gmail.com>
2021-04-24 18:08:46 +0600

    added style of colorbox

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette {...SeedColors[4]}/>
+        <Palette {...SeedColors[1]}/>
       </div>
     );
   }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,11 +6,14 @@ class ColorBox extends Component {
   render() {
     const { background, name } = this.props;
     return (
-      <div className="ColorBox" style={{ background: background }}>
-        <div className="copy-container" />
-        <div className="box-content">
-          <span>{name}</span>
+      <div className="ColorBox" style={{ background }}>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
         </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
