961c918de75b2e1b23e7ceadfa2285f775a1321d
suman Islam <sumunahmed1416@gmail.com>
2021-05-02 12:18:01 +0600

    finish styling minipalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -14,7 +14,11 @@ const styles = {
     },
   },
   colors: {
-    backgroundColor: "gray",
+    backgroundColor: "#dae1e4",
+    height: "120px",
+    width:'100%',
+    borderRadius: '3px',
+    overflow: 'hidden'
   },
   title: {
     display: "flex",
@@ -27,16 +31,30 @@ const styles = {
   },
   emoji: {
     marginLeft: "0.5rem",
-    fontSize: '1.5rem'
+    fontSize: "1.5rem",
+  },
+  miniColorBox: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    marginBottom: '-4px'
   },
 };
 
 function MiniPalette(props) {
-  const { classes, emoji, paletteName } = props;
+  const { classes, emoji, paletteName, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColorBox}
+      style={{ background: color.color }}
+      key={color.name}
+    />
+  ));
   return (
     <div>
       <div className={classes.root}>
-        <div className={classes.colors}></div>
+        <div className={classes.colors}>{miniColorBoxes}</div>
         <h1 className={classes.title}>
           {paletteName} <span className={classes.emoji}>{emoji}</span>
         </h1>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -9,13 +9,13 @@ import MiniPalette from "./MiniPalette";
 const styles = {
   root: {
     backgroundColor: "purple",
-    height: "100%",
+    height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
   },
   container: {
-    width: "50%",
+    width: "65%",
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
