31a2b7626e5ad35a809fcb552132041736dc5c69
SumanIslam <sumanislam@gmail.com>
2021-07-19 12:26:26 +0600

    move draggable-colorbox styles out

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,39 +1,7 @@
 import { withStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { SortableElement } from 'react-sortable-hoc';
-
-const styles = {
-  root: {
-    height: '25%',
-    width: '20%',
-    margin: '0 auto',
-    display: 'inline-block',
-    cursor: 'pointer',
-    position: 'relative',
-    marginBottom: '-5px',
-    '&:hover svg': {
-      color: 'white',
-      transform: 'scale(1.3)',
-    },
-  },
-  boxContent: {
-    position: 'absolute',
-    width: '100%',
-    left: '0',
-    bottom: '0',
-    fontSize: '13px',
-    letterSpacing: '1px',
-    padding: '10px',
-    color: 'rgba(0,0,0,0.5)',
-    textTransform: 'uppercase',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  deleteIcon: {
-    transition: 'all 0.4s ease-in-out',
-  },
-};
+import styles from './styles/DraggableColorBox';
 
 const DraggableColorBox = SortableElement(({ color, name, classes, handleClick }) => (
   <div className={classes.root} style={{ backgroundColor: color }}>
